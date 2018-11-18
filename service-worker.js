e6d66831e0f796bde7ab1e06faf8e2c595152eca
Paul Jensen <paulbjensen@gmail.com>
2018-11-18 13:39:02 +0000

    Style update and added service worker

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// Start
+const CACHE_NAME = 'paulbjensendotcodotuk1541365204561';
+const urlsToCache = [
+  '/',
+  'android-chrome-192x192.png',
+  'apple-touch-icon.png',
+  'browserconfig.xml',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/favicon.ico',
+  'mstile-150x150.png',
+  'safari-pinned-tab.svg',
+  'site.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache =>
+        // Open a cache and cache our files
+        cache.addAll(urlsToCache)
+      )
+      .then(() =>
+        // skipWaiting() forces the waiting ServiceWorker to become the
+        // active ServiceWorker, triggering the onactivate event.
+        // Together with Clients.claim() this allows a worker to take effect
+        // immediately in the client(s).
+        self.skipWaiting()
+      )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // console.log(event.request.url)
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
